4e01917cf6f152980b3c99eba54888617414255d
Andrejus Novatorovas <andrejus.novatorovas@nfq001839.foreman.nfq.lt>
2022-03-30 11:58:21 +0300

    system undefined on typechecking

--- packages/local-client/src/bundler/plugins/typecheck-plugin.ts
+++ packages/local-client/src/bundler/plugins/typecheck-plugin.ts
@@ -1,84 +1,108 @@
 import * as esbuild from 'esbuild-wasm';
 import * as ts from 'typescript';
 
-function compile(fileNames: string[], options: ts.CompilerOptions): void {
-  let program = ts.createProgram(fileNames, options);
-  let emitResult = program.emit();
-
-  let allDiagnostics = ts
-    .getPreEmitDiagnostics(program)
-    .concat(emitResult.diagnostics);
-
-  allDiagnostics.forEach((diagnostic) => {
-    if (diagnostic.file) {
-      let { line, character } = ts.getLineAndCharacterOfPosition(
-        diagnostic.file,
-        diagnostic.start!
-      );
-      let message = ts.flattenDiagnosticMessageText(
-        diagnostic.messageText,
-        '\n'
-      );
-      console.log(
-        `${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`
-      );
-    } else {
-      console.log(
-        ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n')
-      );
-    }
-  });
-
-  let exitCode = emitResult.emitSkipped ? 1 : 0;
-  console.log(`Process exiting with code '${exitCode}'.`);
-  process.exit(exitCode);
-}
+const tsValid = "let x: string  = 'string'";
+const tsUnvalid = `interface A { b: 'c' };
+const e : A = { b: 123 };`;
 
 export const typecheckPlugin = (inputCode: string) => {
   return {
     name: 'typecheck-plugin',
     setup(build: esbuild.PluginBuild) {
-      const source = "let x: string  = 'string'";
-      let result = ts.transpileModule(source, {
+      /* ################################################################################################################################################## */
+      /* SIMPLY TRANSPILING - RETURNS NO DIAGNOSTICS INFO */
+      /* https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API#a-simple-transform-function */
+      let resultValid = ts.transpileModule(tsValid, {
         compilerOptions: { module: ts.ModuleKind.ES2015 },
       });
-      console.log(JSON.stringify(result));
+      console.log('valid ts code', JSON.stringify(resultValid));
+      // OUTPUT:
+      // valid ts code {"outputText":"var x = 'string';\r\n","diagnostics":[]}
 
-      const source2 = `
-      interface A {
-      b: 'c' }
-      
-      const e : A = { b: 123 };
-      show(e.b);`;
-
-      console.log(
-        '##############################PLUGIN###########################'
-      );
-      let result2 = ts.transpileModule(source2, {
+      let resultInvalid = ts.transpileModule(tsUnvalid, {
         compilerOptions: { module: ts.ModuleKind.CommonJS },
       });
-      console.log(JSON.stringify(result2));
+      console.log('invalid ts code with error', JSON.stringify(resultInvalid));
+      // OUTPUT:
+      // invalid ts code with error {"outputText":";\r\nvar e = { b: 123 };\r\n","diagnostics":[]}
 
-      compile(process.argv.slice(2), {
-        noEmitOnError: true,
-        noImplicitAny: true,
-        target: ts.ScriptTarget.ES5,
-        module: ts.ModuleKind.CommonJS,
-      });
+      /* ################################################################################################################################################## */
+      /* TRYING TO TYPECHECK 1 */
+      /* https://convincedcoder.com/2019/01/19/Processing-TypeScript-using-TypeScript/#getting-diagnostics */
+      const program = ts.createProgram(['../../components/add-cell.tsx'], {});
+      const diagnostics = ts.getPreEmitDiagnostics(program);
+
+      for (const diagnostic of diagnostics) {
+        const message = diagnostic.messageText;
+        const file = diagnostic.file;
+        const filename = file?.fileName;
+
+        const lineAndChar = file?.getLineAndCharacterOfPosition(
+          diagnostic.start!!
+        );
+
+        const line = lineAndChar!!.line + 1;
+        const character = lineAndChar!!.character + 1;
+
+        console.log(message);
+        console.log(`(${filename}:${line}:${character})`);
+      }
+      // OUTPUT:
+      // > error: system is undefined 88dc327b-fc00-4c69-8102-24c861d03dfd:8:24
+      // 1 error 88dc327b-fc00-4c69-8102-24c861d03dfd:8:24
+
+      // /* ################################################################################################################################################## */
+      // /* TRYING TO TYPECHECK 2 */
+      // /* https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API#a-minimal-compiler */
+      // function compile(fileNames: string[], options: ts.CompilerOptions): void {
+      //   let program = ts.createProgram(fileNames, options);
+      //   let emitResult = program.emit();
+
+      //   let allDiagnostics = ts
+      //     .getPreEmitDiagnostics(program)
+      //     .concat(emitResult.diagnostics);
+
+      //   allDiagnostics.forEach((diagnostic) => {
+      //     if (diagnostic.file) {
+      //       let { line, character } = ts.getLineAndCharacterOfPosition(
+      //         diagnostic.file,
+      //         diagnostic.start!
+      //       );
+      //       let message = ts.flattenDiagnosticMessageText(
+      //         diagnostic.messageText,
+      //         '\n'
+      //       );
+      //       console.log(
+      //         `${diagnostic.file.fileName} (${line + 1},${
+      //           character + 1
+      //         }): ${message}`
+      //       );
+      //     } else {
+      //       console.log(
+      //         ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n')
+      //       );
+      //     }
+      //   });
+
+      //   let exitCode = emitResult.emitSkipped ? 1 : 0;
+      //   console.log(`Process exiting with code '${exitCode}'.`);
+      //   process.exit(exitCode);
+      // }
+
+      // // process.argv.slice(2) - empty array at runtime? replacing with a relative path to an existing file changes nothing
+      // compile(process.argv.slice(2), {
+      //   noEmitOnError: true,
+      //   noImplicitAny: true,
+      //   target: ts.ScriptTarget.ES5,
+      //   module: ts.ModuleKind.CommonJS,
+      // });
+      // // OUTPUT:
+      // // > error: system is undefined fb9746b8-7ae6-4d9f-9820-c0beb7080297:8:24
+      // // 1 error fb9746b8-7ae6-4d9f-9820-c0beb7080297:8:24
 
       build.onLoad(
         { filter: /(^index\.js$)/ },
         async (args: esbuild.OnLoadArgs) => {
-          const source = "let x: string  = 'string'";
-          console.log(
-            '##############################PLUGIN###########################'
-          );
-
-          let result = ts.transpileModule(source, {
-            compilerOptions: { module: ts.ModuleKind.CommonJS },
-          });
-
-          console.log(JSON.stringify(result));
           return undefined;
         }
       );

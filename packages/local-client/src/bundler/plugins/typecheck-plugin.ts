22a7803aad9b2a5fab4345f6d00fa2e74151336e
Andrejus Novatorovas <andrejus.novatorovas@nfq001839.foreman.nfq.lt>
2022-03-31 15:09:16 +0300

    virtual-tsc library not helping

--- packages/local-client/src/bundler/plugins/typecheck-plugin.ts
+++ packages/local-client/src/bundler/plugins/typecheck-plugin.ts
@@ -1,5 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
 import * as ts from 'typescript';
+import * as path from 'path';
+import { compile } from 'virtual-tsc';
 
 const tsValid = "let x: string  = 'string'";
 const tsUnvalid = `interface A { b: 'c' };
@@ -26,30 +28,30 @@ export const typecheckPlugin = (inputCode: string) => {
       // OUTPUT:
       // invalid ts code with error {"outputText":";\r\nvar e = { b: 123 };\r\n","diagnostics":[]}
 
-      /* ################################################################################################################################################## */
-      /* TRYING TO TYPECHECK 1 */
-      /* https://convincedcoder.com/2019/01/19/Processing-TypeScript-using-TypeScript/#getting-diagnostics */
-      const program = ts.createProgram(['../../components/add-cell.tsx'], {});
-      const diagnostics = ts.getPreEmitDiagnostics(program);
+      // /* ################################################################################################################################################## */
+      // /* TRYING TO TYPECHECK 1 */
+      // /* https://convincedcoder.com/2019/01/19/Processing-TypeScript-using-TypeScript/#getting-diagnostics */
+      // const program = ts.createProgram(['../../components/add-cell.tsx'], {});
+      // const diagnostics = ts.getPreEmitDiagnostics(program);
 
-      for (const diagnostic of diagnostics) {
-        const message = diagnostic.messageText;
-        const file = diagnostic.file;
-        const filename = file?.fileName;
+      // for (const diagnostic of diagnostics) {
+      //   const message = diagnostic.messageText;
+      //   const file = diagnostic.file;
+      //   const filename = file?.fileName;
 
-        const lineAndChar = file?.getLineAndCharacterOfPosition(
-          diagnostic.start!!
-        );
+      //   const lineAndChar = file?.getLineAndCharacterOfPosition(
+      //     diagnostic.start!!
+      //   );
 
-        const line = lineAndChar!!.line + 1;
-        const character = lineAndChar!!.character + 1;
+      //   const line = lineAndChar!!.line + 1;
+      //   const character = lineAndChar!!.character + 1;
 
-        console.log(message);
-        console.log(`(${filename}:${line}:${character})`);
-      }
-      // OUTPUT:
-      // > error: system is undefined 88dc327b-fc00-4c69-8102-24c861d03dfd:8:24
-      // 1 error 88dc327b-fc00-4c69-8102-24c861d03dfd:8:24
+      //   console.log(message);
+      //   console.log(`(${filename}:${line}:${character})`);
+      // }
+      // // OUTPUT:
+      // // > error: system is undefined 88dc327b-fc00-4c69-8102-24c861d03dfd:8:24
+      // // 1 error 88dc327b-fc00-4c69-8102-24c861d03dfd:8:24
 
       // /* ################################################################################################################################################## */
       // /* TRYING TO TYPECHECK 2 */
@@ -99,6 +101,184 @@ export const typecheckPlugin = (inputCode: string) => {
       // // OUTPUT:
       // // > error: system is undefined fb9746b8-7ae6-4d9f-9820-c0beb7080297:8:24
       // // 1 error fb9746b8-7ae6-4d9f-9820-c0beb7080297:8:24
+
+      // /* ################################################################################################################################################## */
+      // /* TRYING TO TYPECHECK 3 */
+      // /* https://stackoverflow.com/questions/52969177/typescript-createprogram-throwing-ts-sys-is-undefined */
+      // /* https://github.com/microsoft/TypeScript/wiki/Using-the-Compiler-API#customizing-module-resolution */
+      // /* create compilerHost manually so that the default implementation is not used */
+
+      // function createCompilerHost(
+      //   options: ts.CompilerOptions,
+      //   moduleSearchLocations: string[]
+      // ): ts.CompilerHost {
+      //   return {
+      //     getSourceFile,
+      //     getDefaultLibFileName: () => 'lib.d.ts',
+      //     writeFile: (fileName, content) => ts.sys.writeFile(fileName, content),
+      //     getCurrentDirectory: () => ts.sys.getCurrentDirectory(),
+      //     getDirectories: (path) => ts.sys.getDirectories(path),
+      //     getCanonicalFileName: (fileName) =>
+      //       ts.sys.useCaseSensitiveFileNames
+      //         ? fileName
+      //         : fileName.toLowerCase(),
+      //     getNewLine: () => ts.sys.newLine,
+      //     useCaseSensitiveFileNames: () => ts.sys.useCaseSensitiveFileNames,
+      //     fileExists,
+      //     readFile,
+      //     resolveModuleNames,
+      //   };
+
+      //   function fileExists(fileName: string): boolean {
+      //     return ts.sys.fileExists(fileName);
+      //   }
+
+      //   function readFile(fileName: string): string | undefined {
+      //     return ts.sys.readFile(fileName);
+      //   }
+
+      //   function getSourceFile(
+      //     fileName: string,
+      //     languageVersion: ts.ScriptTarget,
+      //     onError?: (message: string) => void
+      //   ) {
+      //     const sourceText = ts.sys.readFile(fileName);
+      //     return sourceText !== undefined
+      //       ? ts.createSourceFile(fileName, sourceText, languageVersion)
+      //       : undefined;
+      //   }
+
+      //   function resolveModuleNames(
+      //     moduleNames: string[],
+      //     containingFile: string
+      //   ): ts.ResolvedModule[] {
+      //     const resolvedModules: ts.ResolvedModule[] = [];
+      //     for (const moduleName of moduleNames) {
+      //       // try to use standard resolution
+      //       let result = ts.resolveModuleName(
+      //         moduleName,
+      //         containingFile,
+      //         options,
+      //         {
+      //           fileExists,
+      //           readFile,
+      //         }
+      //       );
+      //       if (result.resolvedModule) {
+      //         resolvedModules.push(result.resolvedModule);
+      //       } else {
+      //         // check fallback locations, for simplicity assume that module at location
+      //         // should be represented by '.d.ts' file
+      //         for (const location of moduleSearchLocations) {
+      //           const modulePath = path.join(location, moduleName + '.d.ts');
+      //           if (fileExists(modulePath)) {
+      //             resolvedModules.push({ resolvedFileName: modulePath });
+      //           }
+      //         }
+      //       }
+      //     }
+      //     return resolvedModules;
+      //   }
+      // }
+
+      // function compile(
+      //   sourceFiles: string[],
+      //   moduleSearchLocations: string[]
+      // ): void {
+      //   const options: ts.CompilerOptions = {
+      //     module: ts.ModuleKind.AMD,
+      //     target: ts.ScriptTarget.ES5,
+      //   };
+      //   const host = createCompilerHost(options, moduleSearchLocations);
+      //   const program = ts.createProgram(sourceFiles, options, host);
+
+      //   /// do something with program...
+      // }
+
+      // compile(process.argv.slice(2), []);
+      // // OUTPUT:
+      // // > error: system is undefined f83f150f-1d53-48e7-b54e-81738cf60696:8:24
+      // // 1 error f83f150f-1d53-48e7-b54e-81738cf60696:8:24
+
+      // /* ################################################################################################################################################## */
+      // /* TRYING TO TYPECHECK 4 */
+      // /* https://github.com/microsoft/TypeScript/issues/29226#issuecomment-450965141 */
+      // /* using an example that works??? */
+
+      // interface File {
+      //   fileName: string;
+      //   content: string;
+      //   sourceFile?: ts.SourceFile;
+      // }
+
+      // const options = {
+      //   module: ts.ModuleKind.CommonJS,
+      //   target: ts.ScriptTarget.ES2015,
+      //   strict: true,
+      //   suppressOutputPathCheck: false,
+      // };
+      // const file: File = { fileName: 'test.ts', content: "let test = 'aa';" };
+
+      // const compilerHost = ts.createCompilerHost(options);
+      // const originalGetSourceFile = compilerHost.getSourceFile;
+      // compilerHost.getSourceFile = (fileName) => {
+      //   console.log(fileName);
+      //   if (fileName === file.fileName) {
+      //     file.sourceFile =
+      //       file.sourceFile ||
+      //       ts.createSourceFile(
+      //         fileName,
+      //         file.content,
+      //         ts.ScriptTarget.ES2015,
+      //         true
+      //       );
+      //     return file.sourceFile;
+      //   } else return originalGetSourceFile.call(compilerHost, fileName, 6);
+      // };
+      // compilerHost.writeFile = (
+      //   fileName,
+      //   data,
+      //   writeByteOrderMark,
+      //   onError,
+      //   sourceFiles
+      // ) => {
+      //   console.log('writeFile =====> fileName: ', fileName, ', data: ', data);
+      // };
+
+      // const program = ts.createProgram([file.fileName], options, compilerHost);
+      // let emitResult = program.emit();
+
+      // // OUTPUT:
+      // // error: system is undefined 1564df11-1a08-421a-8b33-b0495f16c55c:8:24
+      // // 1 error 1564df11-1a08-421a-8b33-b0495f16c55c:8:24
+
+      // /* ################################################################################################################################################## */
+      // /* TRYING TO TYPECHECK 5 */
+      // /* https://github.com/AlCalzone/virtual-tsc */
+      // /* using an example that works??? */
+
+      interface CompileResult {
+        success: boolean;
+        diagnostics: Diagnostic[];
+        result?: string;
+        declarations?: string;
+      }
+
+      interface Diagnostic {
+        type: 'error' | 'warning' | 'message';
+        lineNr: number;
+        charNr: number;
+        sourceLine: string;
+        description: string;
+        annotatedSource: string;
+      }
+
+      const result: CompileResult = compile(tsValid);
+      console.log(result);
+
+      // // OUTPUT:
+      // // > error: Cannot find module 'typescript' 0aa7af02-507f-4476-a967-dcc028250455:8:24
+      // // 1 error 0aa7af02-507f-4476-a967-dcc028250455:8:24
 
       build.onLoad(
         { filter: /(^index\.js$)/ },
